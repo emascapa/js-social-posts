c903fe400e4e5164c3f79a1cfb436da18200a7e8
emascapa <emascapa@gmail.com>
2022-04-12 15:46:14 +0200

    Add like buttons functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ numero di likes.
 
 const posts = [
     {
+        post_id: 1,
         author_name: 'Pippo Baudo',
         author_img: 'https://unsplash.it/50/50',
         post_date: '3/12/2018',
@@ -20,11 +21,12 @@ const posts = [
 
     },
     {
+        post_id: 2,
         author_name: 'Cippa Lippa',
         author_img: 'https://picsum.photos/50/50',
         post_date: '30/10/2019',
         post_content: 'Autem tenetur nulla officiis ratione voluptas incidunt eum a voluptate explicabo?',
-        post_img: 'https://picsum.photos/750/450',
+        post_img: 'https://unsplash.it/750/450',
         likes_counter: 44 
     }
 ];
@@ -33,6 +35,42 @@ const postsContainer = document.querySelector('body > .container > .row');
 
 displayPosts(posts, postsContainer)
 
+const likeButtons = document.querySelectorAll('.like_btn');
+
+likeButtons.forEach((element, i)=> {
+    element.addEventListener('click', function() {
+        //element.classList.toggle('text-primary');
+        
+
+        if (element.clicked !== true) {
+            element.classList.add('text-primary');
+            element.clicked = true; 
+
+            //console.log(`.number_of_likes_${element.post_id}`);
+
+            //console.log(parseInt(document.querySelector(`.number_of_likes_${i+1}`).innerText));
+
+            document.querySelector(`.number_of_likes_${i+1}`).innerHTML = parseInt(document.querySelector(`.number_of_likes_${i+1}`).innerText) + 1;
+            //console.log('mi hai cliccato');
+
+            //aumenta il contatore
+        } else {
+            element.classList.remove('text-primary');
+            element.clicked = false; 
+
+            document.querySelector(`.number_of_likes_${i+1}`).innerHTML = parseInt(document.querySelector(`.number_of_likes_${i+1}`).innerText) - 1;
+
+            //console.log('mi hai DEcliccato');
+        }
+        
+    })
+})
+
+
+/* document.querySelectorAll('.like_btn').addEventListener('click', function() {
+    console.log('mi hai cliccato');
+}) */
+//const likeButtons = 
 
 
 
@@ -40,64 +78,70 @@ displayPosts(posts, postsContainer)
 function displayPosts(postsArray, domElement) {
     
     postsArray.forEach(element => {
-        const htmlContent = `
-        <div class="col">
-            <div class="p-3 rounded rounded-3 bg-light">
-                <div class="author_div d-flex align-items-center mb-3">
-                    <img src=${element.author_img} alt="profile pic" class="rounded-pill">
-                    <div class="ms-3 d-flex flex-column">
-                        <h5 class="m-0">${element.author_name}</h5>
-                        <span>${element.post_date}</span>
-                    </div>
-                </div>
-                <div class="post_div mb-3">
-                    <span class="lead">${element.post_content}</span>
-                    <img src=${element.post_img} alt="" class="mt-3">
-                </div>
-                <div class="likes_div d-flex align-items-center">
-                    <div class="like_btn w-50 d-flex justify-content-center align-itens-center">
-                        <span class=""><i class="fa-solid fa-thumbs-up"></i> Mi Piace</span>
-                    </div>
-                    <div class="w-50 d-flex justify-content-center align-itens-center">
-                        <span>Piace a <span class="fw-bold">${element.likes_counter}</span> persone</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
 
-        domElement.insertAdjacentHTML('beforeend', htmlContent);
-    })
-}
+        //console.log(element.post_img);
+        //console.log(typeof element.post_img);
 
+        let htmlContent;
 
-const pippo = `
+        if (element.post_img !== undefined) {
+
+            //console.log('eccoci 1');
+            htmlContent = `
             <div class="col">
                 <div class="p-3 rounded rounded-3 bg-light">
                     <div class="author_div d-flex align-items-center mb-3">
-                        <img src="https://unsplash.it/50/50" alt="profile pic" class="rounded-pill">
+                        <img src=${element.author_img} alt="profile pic" class="rounded-pill">
                         <div class="ms-3 d-flex flex-column">
-                            <h5 class="m-0">Autore Post</h5>
-                            <span>Data</span>
+                            <h5 class="m-0">${element.author_name}</h5>
+                            <span>${element.post_date}</span>
                         </div>
                     </div>
                     <div class="post_div mb-3">
-                        <span class="lead">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Autem tenetur nulla officiis ratione voluptas incidunt eum a 
-                            voluptate explicabo? Aspernatur cupiditate vero numquam, 
-                            provident dicta adipisci impedit eaque error dolorum.
-                        </span>
-                        <img src="https://unsplash.it/750/450" alt="" class="mt-3">
+                        <span class="lead">${element.post_content}</span>
+                        <img src=${element.post_img} alt="" class="mt-3">
                     </div>
                     <div class="likes_div d-flex align-items-center">
                         <div class="like_btn w-50 d-flex justify-content-center align-itens-center">
-                            <span class="text-primary"><i class="fa-solid fa-thumbs-up"></i> Mi Piace</span>
+                            <span class=""><i class="fa-solid fa-thumbs-up"></i> Mi Piace</span>
                         </div>
                         <div class="w-50 d-flex justify-content-center align-itens-center">
-                            <span>Piace a <span class="fw-bold">80</span> persone</span>
+                            <span>Piace a <span class="fw-bold number_of_likes_${element.post_id}">${element.likes_counter}</span> persone</span>
                         </div>
                     </div>
                 </div>
             </div>
-`;
+            `;
+        } else {
+            //console.log('eccoci 2');
+
+            htmlContent = `
+            <div class="col">
+                <div class="p-3 rounded rounded-3 bg-light">
+                    <div class="author_div d-flex align-items-center mb-3">
+                        <img src=${element.author_img} alt="profile pic" class="rounded-pill">
+                        <div class="ms-3 d-flex flex-column">
+                            <h5 class="m-0">${element.author_name}</h5>
+                            <span>${element.post_date}</span>
+                        </div>
+                    </div>
+                    <div class="post_div mb-3">
+                        <span class="lead">${element.post_content}</span>
+                    </div>
+                    <div class="likes_div d-flex align-items-center">
+                        <div class="like_btn w-50 d-flex justify-content-center align-itens-center">
+                            <span class=""><i class="fa-solid fa-thumbs-up"></i> Mi Piace</span>
+                        </div>
+                        <div class="w-50 d-flex justify-content-center align-itens-center">
+                            <span>Piace a <span class="fw-bold number_of_likes_${element.post_id}">${element.likes_counter}</span> persone</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+ 
+        domElement.insertAdjacentHTML('beforeend', htmlContent);
+
+    })
+}
